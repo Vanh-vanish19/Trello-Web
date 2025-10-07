eadc785ebe3f89f0440b681741831db702aa9514
Vanh190404 <AnhNV.B22PT015@stu.ptit.edu.vn>
2025-10-08 01:32:07 +0700

    Update mui

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -21,7 +21,14 @@ module.exports = {
     'react-hooks/exhaustive-deps': 'warn',
     'react/prop-types': 0,
     'react/display-name': 0,
-
+    //mui
+    'no-restricted-imports': [
+      'error',
+      {
+        'patterns': [{ 'regex': '^@mui/[^/]+$' }]
+      }
+    ],
+    //common
     'no-console': 1,
     'no-lonely-if': 1,
     'no-unused-vars': 1,
