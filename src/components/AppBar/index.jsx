f4c8bf50753d712756c576bcbfcf8c2f61682e2b
Vanh190404 <AnhNV.B22PT015@stu.ptit.edu.vn>
2025-10-11 16:43:27 +0700

    header_coding

--- src/components/AppBar/index.jsx
+++ src/components/AppBar/index.jsx
@@ -1,16 +1,108 @@
 import Box from '@mui/material/Box'
-import ModeSelect from '../../components/ModeSelect'
+import ModeSelect from '~/components/ModeSelect'
+import AppsIcon from '@mui/icons-material/Apps'
+import { ReactComponent as trelloIcon } from '~/assets/trelloIcon.svg'
+import SvgIcon from '@mui/material/SvgIcon'
+import Typography from '@mui/material/Typography'
+import WorkSpace from './Menus/WorkSpace'
+import Recent from './Menus/Recent'
+import Starred from './Menus/Starred'
+import Templates from './Menus/Templates'
+import Button from '@mui/material/Button'
+import SearchIcon from '@mui/icons-material/Search'
+import { styled, alpha } from '@mui/material/styles'
+import InputBase from '@mui/material/InputBase'
+import NotificationsIcon from '@mui/icons-material/Notifications'
+import ToolTip from '@mui/material/Tooltip'
+import Badge from '@mui/material/Badge'
+import HelpOutlineIcon from '@mui/icons-material/HelpOutline'
+import Profiles from './Menus/Profiles'
+
+const Search = styled('div')(({ theme }) => ({
+  position: 'relative',
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.white, 0.05),
+  '&:hover': {
+    backgroundColor: alpha(theme.palette.common.white, 0.1)
+  },
+  marginLeft: 0,
+  width: '100%',
+  [theme.breakpoints.up('sm')]: {
+    marginLeft: theme.spacing(1),
+    width: 'auto'
+  }
+}))
+
+const SearchIconWrapper = styled('div')(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: '100%',
+  position: 'absolute',
+  pointerEvents: 'none',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center'
+}))
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: 'inherit',
+  width: '100%',
+  '& .MuiInputBase-input': {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create('width'),
+    [theme.breakpoints.up('sm')]: {
+      width: '12ch',
+      '&:focus': {
+        width: '20ch'
+      }
+    }
+  }
+}))
 
 function AppBar() {
   return (
-    <Box sx={{
+    <Box px={2} sx={{
       backgroundColor: 'primary.light',
       width: '100%',
       height: (theme) => theme.trello.appBarHeight,
       display: 'flex',
-      alignItems: 'center'
+      alignItems: 'center',
+      justifyContent: 'space-between'
     }}>
-      <ModeSelect/>
+      <Box sx={{ color : 'primary.main', display: 'flex', alignItems: 'center', gap : 2 }}>
+        <AppsIcon />
+        <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
+          <SvgIcon component={trelloIcon} inheritViewBox />
+          <Typography variant='span' sx={{ fontSize: 20, fontWeight: 'bold' }}>Trello</Typography>
+        </Box>
+        <WorkSpace />
+        <Recent />
+        <Starred />
+        <Templates />
+        <Button variant='outlined'>Create</Button>
+      </Box>
+      <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
+        <Search>
+          <SearchIconWrapper>
+            <SearchIcon />
+          </SearchIconWrapper>
+          <StyledInputBase
+            placeholder="Search…"
+            inputProps={{ 'aria-label': 'search' }}
+          />
+        </Search>
+        <ModeSelect/>
+        <ToolTip alt="Notifications" title="Notifications" sx={{ cursor : 'pointer' }}>
+          <Badge color="secondary" variant='dot'>
+            <NotificationsIcon />
+          </Badge>
+        </ToolTip>
+        <ToolTip alt="Help" title="Help" sx={{ cursor : 'pointer' }}>
+          <HelpOutlineIcon/>
+        </ToolTip>
+        <Profiles/>
+      </Box>
     </Box>
   )
 }
