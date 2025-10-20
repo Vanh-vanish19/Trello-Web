0a3261d3fbfb8ab04e1c2c216dc741c1d4d1861a
Vanh190404 <AnhNV.B22PT015@stu.ptit.edu.vn>
2025-10-20 23:44:00 +0700

    done_drag_drop_Card

--- src/utils/formatters.js
+++ src/utils/formatters.js
@@ -3,3 +3,12 @@ export const capitalizeFirstLetter = (val) => {
   if (!val) return ''
   return `${val.charAt(0).toUpperCase()}${val.slice(1)}`
 }
+
+export const genPlaceholderCard = (column) => {
+  return {
+    _id: `${column._id}-placeholder-card`,
+    boardId: `${column.boardId}`,
+    columnId: `${column._id}`,
+    FE_PLACEHOLDER_CARD: true
+  }
+}
