ff2a2f06e6f0b1d4e280beb32f0bd67316b9dd75
Vanh190404 <AnhNV.B22PT015@stu.ptit.edu.vn>
2025-11-09 22:40:54 +0700

    fix build_mode dev production

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,1 +1,9 @@
-export const API_ROOT = 'http://localhost:8021'
+let apiRoot = ''
+if (process.env.BUILD_MODE == 'dev') {
+  apiRoot = 'http://localhost:8017'
+}
+if (process.env.BUILD_MODE == 'production') {
+  apiRoot = 'https://trello-api-1-gpkm.onrender.com'
+}
+console.log(apiRoot)
+export const API_ROOT = apiRoot
