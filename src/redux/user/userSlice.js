38f83afda1f0c17865ebc1a8e3c4525739d5af4d
Vanh190404 <AnhNV.B22PT015@stu.ptit.edu.vn>
2025-11-13 15:53:42 +0700

    update verified and login

---
+++ src/redux/user/userSlice.js
@@ -0,0 +1,41 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
+import authorizeAxiosInstance from '~/utils/authorizeAxios'
+import { API_ROOT } from '~/utils/constants'
+
+const initialState = {
+  currentUser: null
+}
+
+// hành động call api dể update data vào redux, dùng middleware createAsyncThunk với extraReducers
+export const loginUserAPI = createAsyncThunk(
+  'user/loginUserAPI',
+  async (data) => {
+    const response = await authorizeAxiosInstance.post(`${API_ROOT}/v1/users/login`, data)
+    return response.data
+  }
+)
+
+
+// init Slice in redux store
+export const userSlice = createSlice({
+  name: 'user',
+  initialState,
+  // reducers xử lý đồng bộ
+  reducers: {
+  },
+  // extraReducres xl bdb
+  extraReducers: (builder) => {
+    builder.addCase(loginUserAPI.fulfilled, (state, action) => {
+      // action.payload : reponse.data
+      const user = action.payload
+      //update currentUser
+      state.currentUser = user
+    })
+  }
+})
+
+export const selectCurrentUser = (state) => {
+  return state.user.currentUser
+}
+
+export const userReducer = userSlice.reducer
