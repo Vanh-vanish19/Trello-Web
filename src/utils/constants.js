cd45e7156bebd2c39cf90b0e87cbed1391aa55b2
Vanh190404 <AnhNV.B22PT015@stu.ptit.edu.vn>
2025-11-23 22:04:40 +0700

    update url

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,7 +3,7 @@ if (process.env.BUILD_MODE == 'dev') {
   apiRoot = 'http://localhost:8022'
 }
 if (process.env.BUILD_MODE == 'production') {
-  apiRoot = 'https://trello-web-puce-mu.vercel.app'
+  apiRoot = 'https://trello-api-1-gpkm.onrender.com'
 }
 export const API_ROOT = apiRoot
 
